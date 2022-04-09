0f1692553ada259b8dfd1c9928a90ae838802c19
Eric Lowe <ericlowe25@gmail.ocm>
2022-04-09 12:52:58 -0400

    not done

--- index.js
+++ index.js
@@ -12,86 +12,99 @@
 //parking/transportation; uber stops, public transportation
 
 //build leaflet map
+// map object
+const myMap = {
+	coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
 
-const userMap = {
-    //Grab user location
-   userPosition: [],
-   userLocalMap:  {},
-   userMarker: {},
-   locations: [],
-   locationMarkers: {},
+	// build leaflet map
+	buildMap() {
+		this.map = L.map('map', {
+		center: this.coordinates,
+		zoom: 11,
+		});
+		// add openstreetmap tiles
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		minZoom: '15',
+		}).addTo(this.map)
+		// create and add geolocation marker
+		const marker = L.marker(this.coordinates)
+		marker
+		.addTo(this.map)
+		.bindPopup('<p1><b>You are here</b><br></p1>')
+		.openPopup()
+	},
 
-   initializeMap() {
-       this.userLocalMap = L.map('map', {center: this.userPosition, zoom: 11,});
-   
-       // Add OpenStreetMap tiles:
-       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-           minZoom: '10',
-       }).addTo(this.userLocalMap)
-   
-       //Add user location marker
-       const marker = L.marker(this.userPosition)
-       marker.addTo(this.userLocalMap).bindPopup('<p1><b>Your are here</b></p1>').openPopup()
-   },
-
-   addBusinesLocations(){
-       this.locations.forEach((location) => {
-           userMap.locationMarkers = L.marker([location.lat, location.long])
-           userMap.locationMarkers.addTo(userMap.userLocalMap).bindPopup(`<p1><b>${location.name}</b></p1>`).openPopup()
-       })
-   },
-
-
+	// add business markers
+	addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	},
 }
 
-//Get user location 
-async function getUserPosition(){
-   let pos = await new Promise((resolve, reject) => {navigator.geolocation.getCurrentPosition(resolve, reject);});
-   console.log(pos.coords)
-   return [pos.coords.latitude, pos.coords.longitude]
+// get coordinates via geolocation api
+async function getCoords(){
+	const pos = await new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	});
+	return [pos.coords.latitude, pos.coords.longitude]
 }
 
-//Wait for page load
+// get foursquare businesses
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3TwTLp/PEtwqDA0+0AU3MuV1uGoi/4cJtd1B28ahOiC8='
+		}
+	}
+	let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
+// process foursquare array
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
+
+
+// event handlers
+// window load
 window.onload = async () => {
-   let coords = await getUserPosition()
-   userMap.userPosition = coords
-   userMap.initializeMap()
+	const coords = await getCoords()
+	myMap.coordinates = coords
+	myMap.buildMap()
 }
 
-//Fetch Foursquare data via API
-async function getFoursquareData(category){
-   const options = {
-       method: 'GET',
-       headers: {
-       Accept: 'application/json',
-       Authorization: 'fsq3TwTLp/PEtwqDA0+0AU3MuV1uGoi/4cJtd1B28ahOiC8='
-       }
-   }
-   let lat = userMap.userPosition[0]
-   let long = userMap.userPosition[1]
-
-   
-   let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
-   let data = await response.json()
-   let localBusinesses = data.results
-
-   return localBusinesses
-}
-
-
-let locationButtons = document.querySelectorAll(".locationButton")
-locationButtons.forEach((location) => { 
-   location.addEventListener('click', async () => {
-       let category = location.textContent;
-       let selectionData = await getFoursquareData(category)
-       
-       selectionData.forEach((location) => {
-           userMap.locations.push({name: location.name, lat: location.geocodes.main.latitude, long: location.geocodes.main.longitude})
-       })
-       userMap.addBusinesLocations()
-
-   })
+// business submit button
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+	let business = document.getElementById('business').value
+	let data = await getFoursquare(business)
+	myMap.businesses = processBusinesses(data)
+	myMap.addMarkers()
 })
-
-
